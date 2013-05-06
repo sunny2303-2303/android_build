ed1ca96b24224b095429355e393e5126a4f3da45
sunny2303-2303 <sunilpradhan23@yahoo.com>
2013-05-06 16:49:11 +0400

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -143,7 +143,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-linux-android-4.6/bin
             ;;
-        arm) toolchaindir=arm/arm-linux-androideabi-4.6/bin
+        arm) toolchaindir=arm/android-toolchain-eabi-linaro/bin
             ;;
         mips) toolchaindir=mips/mipsel-linux-android-4.6/bin
             ;;
